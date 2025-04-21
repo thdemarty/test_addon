60ed03f33719a529f5e59be9475320811df82937
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-04-21 19:52:28 +0200

    up.

--- domiot_test/backend/app.py
+++ domiot_test/backend/app.py
@@ -2,32 +2,13 @@ from flask import Flask, request
 from flask import render_template
 from utils import get_user_role
 
-from logging.config import dictConfig
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://sys.stderr',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
 
 app = Flask(__name__)
-
-
 app.config.from_prefixed_env()
 
 
 @app.route("/")
-def hello_world():
+def home():
     if not app.config["core_addon_hostname"] or app.config["core_addon_hostname"] == "":
         return render_template("missconfig.html")
 
@@ -38,3 +19,6 @@ def hello_world():
         "role": get_user_role(username.strip()),
     }
     return render_template("index.html", **context)
+
+
+
