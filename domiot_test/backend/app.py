ead8a8388f199fe1e16fb6b6ae031e9991de4084
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-04-23 14:32:22 +0200

    Add debug in configuration

--- domiot_test/backend/app.py
+++ domiot_test/backend/app.py
@@ -6,14 +6,14 @@ from utils import get_user_role
 app = Flask(__name__)
 app.config.from_prefixed_env()
 
-
 @app.route("/")
 def home():
     if not app.config["core_addon_hostname"] or app.config["core_addon_hostname"] == "":
         return render_template("missconfig.html")
 
-
     username = request.headers.get("X-Remote-User-Name")
+    if app.config['DEBUG']:
+        username = "admin"
     context = {
         "username": username,
         "role": get_user_role(username.strip()),
--- domiot_test/backend/utils.py
+++ domiot_test/backend/utils.py
@@ -8,15 +8,19 @@ def get_user_role(username=""):
     core_addon = app.config.get("core_addon_hostname")
     api_url = f"http://{core_addon}:8099/api/users/{username}"
     print("API URL: ", api_url)
-    with request.urlopen(api_url) as response:
-        data = response.read()
-        print("Response: ", data)
-        # Decode bytes to string
-        data = data.decode("utf-8")
-        # Convert JSON string to Python dictionary
-        data = json.loads(data)
+    if not app.config["DEBUG"]:
+        with request.urlopen(api_url) as response:
+            data = response.read()
+            print("Response: ", data)
+            # Decode bytes to string
+            data = data.decode("utf-8")
+            # Convert JSON string to Python dictionary
+            data = json.loads(data)
 
-        return data["domiot_role"]
+            return data["domiot_role"]
+    else:
+        # Simulate a response for testing purposes
+        return "patient"
 
 
             
